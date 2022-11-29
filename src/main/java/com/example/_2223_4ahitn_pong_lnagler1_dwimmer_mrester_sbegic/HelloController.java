d0a7bc9212af5d5e0f71fc8cc9d879558e288346
Daniel <dwimmer1@htl-steyr.ac.at>
2022-11-29 09:41:16 +0100

    Files yes

---
+++ src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/HelloController.java
@@ -0,0 +1,14 @@
+package com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic to javafx.fxml;
+    exports com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic;
+}
