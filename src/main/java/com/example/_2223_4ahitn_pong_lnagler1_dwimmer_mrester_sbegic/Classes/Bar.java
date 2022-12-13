0335531ab3642dd3b3253f2b6b6f9602cb587c4f
manue <mrester@htl-steyr.ac.at>
2022-12-13 09:29:44 +0100

    created bars for Playfield

---
+++ src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/Classes/Bar.java
@@ -0,0 +1,43 @@
+package com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.Classes;
+
+import com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.model.PlayField;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Bar {
+    private double length;
+    private double width;
+    private final Color color;
+
+    //PlayField playField = PlayField.getInstance();
+
+    public Bar() {
+        this.length = 0;
+        this.width = 0;
+        this.color = Color.WHITE;
+    }
+
+    public void setBar(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(this.color);
+        graphicsContext.fillRect(0, 0, this.width, this.length);
+        graphicsContext.setFill(this.color);
+        graphicsContext.fillRect(100, 100, this.width, this.length);
+    }
+
+    public double getLenght() {
+        return this.length;
+    }
+
+    public void setLenght(double lenght) {
+        this.length = lenght;
+    }
+
+    public double getWidht() {
+        return width;
+    }
+
+    public void setWidht(int widht) {
+        this.width = widht;
+    }
+}
--- src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/Controller/GameController.java
+++ src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/Controller/GameController.java
@@ -1,10 +1,12 @@
 package com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.Controller;
 
+import com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.Classes.Bar;
 import com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.model.PlayField;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -15,6 +17,8 @@ public class GameController {
     private int width;
     private int height;
     private GraphicsContext graphicsContext;
+
+    private Bar bar = new Bar();
 
     public void loadPlayField() {
         setWidthAndHeight();
@@ -35,20 +39,30 @@ public class GameController {
     public void startGame() {
         PlayField playField = new PlayField();
         playField.setBackground(graphicsContext, width, height);
+        bar.setBar(graphicsContext);
     }
 
     public void setWidthAndHeight() {
         if (screenWidth <= 800 && screenHeight <= 600) {
             this.width = 600;
             this.height = 400;
+            setBarMeassures(height, width);
         } else if (screenWidth <= 1280 && screenHeight <= 768) {
             this.width = 800;
             this.height = 600;
+            setBarMeassures(height, width);
         } else if (screenWidth <= 1920 && screenHeight <= 1080) {
             this.width = 1000;
             this.height = 800;
+            setBarMeassures(height, width);
         }
         System.out.println(screenWidth);
+        System.out.println(bar.getLenght());
+        System.out.println(bar.getWidht());
     }
 
+    public void setBarMeassures(int length, int width){
+        bar.setLenght(length/4.5);
+        bar.setWidht(width/33);
+    }
 }
