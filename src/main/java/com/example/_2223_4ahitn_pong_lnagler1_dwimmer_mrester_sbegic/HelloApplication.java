131547dfe19279b780da11091db8919415b7609d
sbegic <sbegic2003@gmail.com>
2022-12-20 10:15:18 +0100

    Implemented the Menue FXML and the functions. Style.css was implemented for design purpose. Player.java for saving what name and field color the players have chosen.

--- src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/HelloApplication.java
+++ src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/HelloApplication.java
@@ -4,16 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menue-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
