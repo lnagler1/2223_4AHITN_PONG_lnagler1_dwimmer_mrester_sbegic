db484d3058c0f450cc17227c7c52480360fc4921
Daniel <dwimmer1@htl-steyr.ac.at>
2023-01-17 08:53:13 +0100

    BarImplement with Check

--- src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/model/CheckScore.java
+++ src/main/java/com/example/_2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic/model/CheckScore.java
@@ -1,29 +1,33 @@
 package com.example._2223_4ahitn_pong_lnagler1_dwimmer_mrester_sbegic.model;
 
 public class CheckScore {
-/*
+
     Ball ballPos = new Ball();
-    Bar barPos = new Bar();
+
+
     int scoreP2 = 0;
     int scoreP1 = 0;
     boolean gameStarted;
 
-    public void checkIfScored() {
-        if (ballPos.getxBallPostition() < barPos.getLeftRectXCord() - barPos.getWidht()) {
+    public void checkIfScored(int leftRectXCord, int rightRectXCord, int getWidht) {
+//        if (ballPos.getxBallPostition() < barPos.getLeftRectXCord() - barPos.getWidht()) {
             scoreP2++;
             gameStarted = false;
-        }
-        if (ballPos.getxBallPostition() < barPos.getRightRectXCord() - barPos.getWidht()) {
+  //      }
+    //    if (ballPos.getxBallPostition() < barPos.getRightRectXCord() - barPos.getWidht()) {
             scoreP1++;
             gameStarted = false;
-        }
+      //  }
     }
 
     public Ball getBallPos() {
         return ballPos;
     }
 
+    /*
     public Bar getBarPos() {
         return barPos;
-    }*/
+    }
+
+     */
 }
